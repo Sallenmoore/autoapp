6cfef856528a9fe2bc54abe2441553ff2178bd83
slgy <me@nowhere.com>
2023-04-12 16:16:54 -0400

    updated

--- app/config.py
+++ app/config.py
@@ -1,4 +1,3 @@
-import logging
 import os
 
 
@@ -6,20 +5,18 @@ import os
 #                         CONFIGURATION                         #
 #################################################################
 class Config:
-    APP_NAME = os.environ.get("APP_NAME", __name__)
+    APP_NAME = os.environ.get("app", __name__)
     HOST = os.environ.get("HOST", "0.0.0.0")
     PORT = os.environ.get("PORT", 80)
     SECRET_KEY = os.environ.get("SECRET_KEY", "NATASHA")
 
 
 class ProductionConfig(Config):
-    LOG_LEVEL = logging.ERROR
     DEBUG = False
     TESTING = False
 
 
 class DevelopmentConfig(Config):
-    LOG_LEVEL = logging.INFO
     DEBUG = True
     TESTING = True
     # TRAP_HTTP_EXCEPTIONS = True
